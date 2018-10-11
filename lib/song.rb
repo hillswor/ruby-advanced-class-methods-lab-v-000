b5609b98c8eff7541c2cdff440ca6216f6f36563
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-11 02:38:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   def self.new_from_filename(name)
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (filename.split(" - ")[0])
+    song.artist_name = (name.split(" - ")[0])
     #initializes a song and artist_name based on the filename format
   end
 
